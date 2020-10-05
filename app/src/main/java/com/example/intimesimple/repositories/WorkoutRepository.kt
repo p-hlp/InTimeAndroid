fd17561414bb9e0e1e54c578f9bb0758f57e9c48
p-hlp <ammunitionrec@gmail.com>
2020-10-05 23:24:41 +0200

    add compose dependencies

--- app/src/main/java/com/example/intimesimple/repositories/WorkoutRepository.kt
+++ app/src/main/java/com/example/intimesimple/repositories/WorkoutRepository.kt
@@ -1,13 +1,13 @@
 package com.example.intimesimple.repositories
 
+import com.example.intimesimple.data.local.Workout
 import com.example.intimesimple.data.local.WorkoutDao
 
 class WorkoutRepository constructor(
     private val workoutDao: WorkoutDao
 ){
-
-
-
-
-
+    suspend fun insertWorkout(workout: Workout) = workoutDao.insertWithTimestamp(workout)
+    suspend fun deleteWorkout(workout: Workout) = workoutDao.deleteWorkout(workout)
+    suspend fun updateWorkout(workout: Workout) = workoutDao.updateWorkout(workout)
+    fun getAllWorkouts() = workoutDao.getAllWorkouts()
 }
