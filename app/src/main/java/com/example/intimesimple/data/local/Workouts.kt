23c08de70a4c05f5bbc6b119648ca4fd6ca0cb16
p-hlp <ammunitionrec@gmail.com>
2020-10-06 13:26:40 +0200

    add default workouts for mocking ui

---
+++ app/src/main/java/com/example/intimesimple/data/local/Workouts.kt
@@ -0,0 +1,10 @@
+package com.example.intimesimple.data.local
+
+val defaultWorkouts = listOf(
+        Workout(0,"15min Posture", 35000L, 15000L, 18),
+        Workout(1,"Morning Yoga", 30000L, 15000L, 12),
+        Workout(2,"Upper Body", 40000L, 20000L, 8),
+        Workout(3,"Lower Body", 40000L, 20000L, 8),
+        Workout(4,"Core Routine", 35000L, 25000L, 9),
+        Workout(5,"Conditioning", 50000L, 20000L, 5),
+)
--- app/src/main/java/com/example/intimesimple/ui/composables/WorkoutItem.kt
+++ app/src/main/java/com/example/intimesimple/ui/composables/WorkoutItem.kt
@@ -1,2 +1,25 @@
 package com.example.intimesimple.ui.composables
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.ui.tooling.preview.Preview
+import com.example.intimesimple.data.local.Workout
+import com.example.intimesimple.data.local.defaultWorkouts
+
+@Composable
+fun WorkoutItem(workout: Workout, modifier: Modifier = Modifier){
+    
+}
+
+
+@Preview
+@Composable
+fun WorkoutItemPreview(){
+
+    ThemedPreview(
+            darkTheme = true,
+            children = {
+                WorkoutItem(workout = defaultWorkouts[0])
+            }
+    )
+}
