6a8a534a61ff2cb9c4bf1f84c497a93fa9d2409b
p-hlp <ammunitionrec@gmail.com>
2020-10-06 13:16:25 +0200

    add timerservice class

---
+++ app/src/main/java/com/example/intimesimple/services/TimerService.kt
@@ -0,0 +1,8 @@
+package com.example.intimesimple.services
+
+import androidx.lifecycle.LifecycleService
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TimerService: LifecycleService(){
+}
