9763b26c79fcc1ff00449d9bf1e46a38c616f22c
p-hlp <ammunitionrec@gmail.com>
2020-10-06 13:17:19 +0200

    add timer utils

---
+++ app/src/main/java/com/example/intimesimple/utils/TimerUtils.kt
@@ -0,0 +1,23 @@
+package com.example.intimesimple.utils
+
+import java.util.concurrent.TimeUnit
+
+fun getFormattedStopWatchTime(ms: Long): String{
+    var milliseconds = ms
+
+    // Convert to hours
+    val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+    milliseconds -= TimeUnit.HOURS.toMillis(hours)
+
+    // Convert to minutes
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+    milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+
+    // Convert to seconds
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+
+    // Build formatted String
+    return "${if(hours < 10) "0" else ""}$hours : " +
+            "${if(minutes < 10) "0" else ""}$minutes : " +
+            "${if(seconds < 10) "0" else ""}$seconds"
+}
