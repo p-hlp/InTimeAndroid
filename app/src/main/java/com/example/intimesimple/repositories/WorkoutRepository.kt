d52538903dd6ff2351ff4375545ad5e97a20414f
p-hlp <ammunitionrec@gmail.com>
2020-10-08 15:33:07 +0200

    refactor assistedinject

--- app/src/main/java/com/example/intimesimple/repositories/WorkoutRepository.kt
+++ app/src/main/java/com/example/intimesimple/repositories/WorkoutRepository.kt
@@ -13,4 +13,5 @@ class WorkoutRepository @Inject constructor(
     suspend fun updateWorkoutLastCompletion(workout: Workout)
             = workoutDao.updateWithLastCompletion(workout)
     fun getAllWorkouts() = workoutDao.getAllWorkouts()
+    fun getWorkout(wId: Long) = workoutDao.getWorkoutWithId(wId)
 }
--- app/src/main/java/com/example/intimesimple/ui/viewmodels/WorkoutDetailViewModel.kt
+++ app/src/main/java/com/example/intimesimple/ui/viewmodels/WorkoutDetailViewModel.kt
@@ -1,27 +1,28 @@
 package com.example.intimesimple.ui.viewmodels
 
-import androidx.hilt.Assisted
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.intimesimple.repositories.WorkoutRepository
+import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
-import javax.inject.Inject
+
 
 class WorkoutDetailViewModel @AssistedInject constructor(
-    val workoutRepository: WorkoutRepository,
-    @Assisted private val wId: Long
-) : ViewModel(){
+    workoutRepository: WorkoutRepository,
+    @Assisted private val workoutId: Long
+) : ViewModel() {
+
+    val workout = workoutRepository.getWorkout(workoutId)
 
     @AssistedInject.Factory
     interface AssistedFactory {
-        fun create(wId: Long): WorkoutDetailViewModel
+        fun create(workoutId: Long): WorkoutDetailViewModel
     }
 
     companion object {
         fun provideFactory(
-                assistedFactory: AssistedFactory,
-                workoutId: Long
+            assistedFactory: AssistedFactory,
+            workoutId: Long
         ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
