81640dcc87c1442d0c2038e9297bc2c7052ed027
p-hlp <ammunitionrec@gmail.com>
2020-10-11 15:59:41 +0200

    add constraintsets for portrait and landscape layouts

--- app/src/main/java/com/example/intimesimple/ui/composables/TestScreen.kt
+++ app/src/main/java/com/example/intimesimple/ui/composables/TestScreen.kt
@@ -20,7 +20,12 @@ import com.example.intimesimple.utils.Constants
 import com.example.intimesimple.utils.getFormattedStopWatchTime
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.WithConstraints
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.ConfigurationAmbient
+import androidx.compose.ui.platform.LayoutDirectionAmbient
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.LayoutDirection
 import com.example.intimesimple.data.local.Workout
 
 @Composable
@@ -80,112 +85,169 @@ fun TestScreenContent(
     val screenWidth = configuration.screenWidthDp
     val buttonWidth = 0.3f * screenWidth
 
-    ConstraintLayout(modifier = modifier) {
-        val buttonRow = createRef()
-        val timerText = createRef()
-        val stateText = createRef()
-        val repText = createRef()
-        val progCircle = createRef()
+    WithConstraints(modifier) {
 
-        val buttonModifier = Modifier.width(buttonWidth.dp)
-        Text(
-                text = (if (timerState == TimerState.EXPIRED)
-                    getFormattedStopWatchTime(exTimeInMillis)
-                else getFormattedStopWatchTime(timeInMillis)),
-                modifier = Modifier.constrainAs(timerText){
-                    top.linkTo(parent.top, 172.dp)
-                    centerHorizontallyTo(parent)
-                },
-                color = Color.White,
-                style = typography.h2
-        )
+        val constraints = if (screenWidth.dp < 600.dp){
+            portraitConstraints()
+        }
+        else landscapeConstraints()
 
-        TimerCircle(
-                modifier = Modifier.constrainAs(progCircle){
-                    top.linkTo(parent.top, 8.dp)
-                },
-                elapsedTime = if(timerState == TimerState.EXPIRED) exTimeInMillis else progressTime,
-                totalTime = exTimeInMillis
-        )
-        Text(
-                text = timerState.stateName,
-                modifier = Modifier.constrainAs(stateText){
-                    top.linkTo(timerText.bottom,8.dp)
-                    centerHorizontallyTo(parent)
-                },
-                color = Color.White,
-                style = typography.h5
-        )
-
-        Text(
-                text = "${
-                    if (timerState == TimerState.EXPIRED)
-                        repCount ?: ""
-                    else timerRepCount
-                }",
-                modifier = Modifier.constrainAs(repText){
-                    top.linkTo(stateText.bottom, 4.dp)
-                    centerHorizontallyTo(parent)
-                },
-                color = Color.White,
-                style = typography.h5
-        )
+        ConstraintLayout(modifier = modifier, constraintSet = constraints) {
 
 
-        Row(
-                modifier = Modifier.fillMaxWidth()
-                        .constrainAs(buttonRow){
-                            bottom.linkTo(parent.bottom, 64.dp)
-                        },
-                horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            timerState.let {
-                when (it) {
-                    TimerState.EXPIRED -> {
-                        Button(
-                                onClick = { sendCommand(Constants.ACTION_START) },
-                                shape = RoundedCornerShape(50),
-                                modifier = buttonModifier
-                        ) {
-                            Text("Start")
+            val buttonModifier = Modifier.width(buttonWidth.dp)
+            Text(
+                    text = (if (timerState == TimerState.EXPIRED)
+                        getFormattedStopWatchTime(exTimeInMillis)
+                    else getFormattedStopWatchTime(timeInMillis)),
+                    color = Color.White,
+                    style = typography.h2,
+                    modifier = Modifier.layoutId("timerText")
+            )
+
+            // Only show in portrait
+            if(screenWidth.dp < 600.dp){
+                TimerCircle(
+                        elapsedTime = if(timerState == TimerState.EXPIRED) exTimeInMillis else progressTime,
+                        totalTime = exTimeInMillis,
+                        modifier = Modifier.layoutId("progCircle")
+                )
+            }
+
+            Text(
+                    text = timerState.stateName,
+                    color = Color.White,
+                    style = typography.h5,
+                    modifier = Modifier.layoutId("stateText")
+            )
+
+            Text(
+                    text = "${
+                        if (timerState == TimerState.EXPIRED)
+                            repCount ?: ""
+                        else timerRepCount
+                    }",
+                    color = Color.White,
+                    style = typography.h5,
+                    modifier = Modifier.layoutId("repText")
+            )
+
+
+            Row(
+                    modifier = Modifier
+                            .fillMaxWidth()
+                            .layoutId("buttonRow"),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                timerState.let {
+                    when (it) {
+                        TimerState.EXPIRED -> {
+                            Button(
+                                    onClick = { sendCommand(Constants.ACTION_START) },
+                                    shape = RoundedCornerShape(50),
+                                    modifier = buttonModifier
+                            ) {
+                                Text("Start")
+                            }
                         }
-                    }
-                    TimerState.RUNNING -> {
-                        Button(
-                                onClick = { sendCommand(Constants.ACTION_PAUSE) },
-                                shape = RoundedCornerShape(50),
-                                modifier = buttonModifier
-                        ) {
-                            Text("Pause")
-                        }
+                        TimerState.RUNNING -> {
+                            Button(
+                                    onClick = { sendCommand(Constants.ACTION_PAUSE) },
+                                    shape = RoundedCornerShape(50),
+                                    modifier = buttonModifier
+                            ) {
+                                Text("Pause")
+                            }
 
-                        Button(
-                                onClick = { sendCommand(Constants.ACTION_CANCEL) },
-                                shape = RoundedCornerShape(50),
-                                modifier = buttonModifier
-                        ) {
-                            Text("Cancel")
+                            Button(
+                                    onClick = { sendCommand(Constants.ACTION_CANCEL) },
+                                    shape = RoundedCornerShape(50),
+                                    modifier = buttonModifier
+                            ) {
+                                Text("Cancel")
+                            }
                         }
-                    }
-                    TimerState.PAUSED -> {
-                        Button(
-                                onClick = { sendCommand(Constants.ACTION_RESUME) },
-                                shape = RoundedCornerShape(50),
-                                modifier = buttonModifier
-                        ) {
-                            Text("Resume")
-                        }
+                        TimerState.PAUSED -> {
+                            Button(
+                                    onClick = { sendCommand(Constants.ACTION_RESUME) },
+                                    shape = RoundedCornerShape(50),
+                                    modifier = buttonModifier
+                            ) {
+                                Text("Resume")
+                            }
 
-                        Button(
-                                onClick = { sendCommand(Constants.ACTION_CANCEL) },
-                                shape = RoundedCornerShape(50),
-                                modifier = buttonModifier
-                        ) {
-                            Text("Cancel")
+                            Button(
+                                    onClick = { sendCommand(Constants.ACTION_CANCEL) },
+                                    shape = RoundedCornerShape(50),
+                                    modifier = buttonModifier
+                            ) {
+                                Text("Cancel")
+                            }
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+private fun portraitConstraints(): ConstraintSet{
+    return ConstraintSet {
+        val buttonRow = createRefFor("buttonRow")
+        val timerText = createRefFor("timerText")
+        val stateText = createRefFor("stateText")
+        val repText = createRefFor("repText")
+        val progCircle = createRefFor("progCircle")
+
+        constrain(progCircle){
+            top.linkTo(parent.top, 8.dp)
+        }
+
+        constrain(buttonRow){
+            bottom.linkTo(parent.bottom, 64.dp)
+        }
+
+        constrain(timerText){
+            top.linkTo(parent.top, 172.dp)
+            centerHorizontallyTo(parent)
+        }
+
+        constrain(stateText){
+            top.linkTo(timerText.bottom, 16.dp)
+            centerHorizontallyTo(parent)
+        }
+
+        constrain(repText){
+            top.linkTo(stateText.bottom, 8.dp)
+            centerHorizontallyTo(parent)
+        }
+    }
+}
+
+private fun landscapeConstraints(): ConstraintSet{
+    return ConstraintSet {
+        val buttonRow = createRefFor("buttonRow")
+        val timerText = createRefFor("timerText")
+        val stateText = createRefFor("stateText")
+        val repText = createRefFor("repText")
+
+        constrain(buttonRow){
+            bottom.linkTo(parent.bottom, 32.dp)
+        }
+
+        constrain(timerText){
+            top.linkTo(parent.top, 32.dp)
+            centerHorizontallyTo(parent)
+        }
+
+        constrain(stateText){
+            top.linkTo(timerText.bottom, 8.dp)
+            centerHorizontallyTo(parent)
+        }
+
+        constrain(repText){
+            top.linkTo(stateText.bottom, 8.dp)
+            centerHorizontallyTo(parent)
         }
     }
 }
